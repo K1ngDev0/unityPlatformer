19fe959257475bf6c6b632ed4d5daa87742565e4
K1ngDev0 <zijlmanslars@gmail.com>
2024-03-20 17:00:49 +0100

    Building System (almost done)
    Overlapping is gefixt. Je kan niet meer overlappen met andere colliders (players, andere buildings, etc.) Ook krijg je een border te zien waar je wel en niet kan plaatsen. Daarnaast heb ik ook nog een reach system toegevoegd. Je kan zelf de reach aanpassen van hoever je kan bouwen.
    
    bug: je kan een building ergens onder aan de screen plaatsen omdat je op de knop wil klikken (hij creeërd er dus nog een extra)

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -8,39 +8,27 @@ using UnityEngine.UI;
 
 public class BuildingSystem : MonoBehaviour
 {
-    public GameObject buildingPrefab;
+
     private GameObject buildingPrefabClone;
+    public Sprite canBuildSprite;
+    public Sprite cantBuildSprite;
+
+    private GameObject player;
     
     GameObject clonesLocation;
-
-    SpriteRenderer buildingPrefabSprite;
-    [SerializeField] Image UIBuilding;
-
     public float gridSize = 1f;
 
     [SerializeField] private bool isPlacing = false;
+    [SerializeField] private float playerReach = 5f;
 
     private void Start()
     {
-        UIBuilding = UIBuilding.GetComponent<Image>();
-        buildingPrefabSprite = buildingPrefab.GetComponent<SpriteRenderer>();
-        UIBuilding.sprite = buildingPrefabSprite.sprite;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Awake()
     {
         clonesLocation = GameObject.FindGameObjectWithTag("buildingsParent");
-    }
-
-    public void onButtonChoice()
-    {
-        if (!isPlacing)
-        {
-            buildingPrefabClone = Instantiate(buildingPrefab, (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
-            buildingPrefabClone.name = buildingPrefab.name;
-            buildingPrefabClone.transform.parent = clonesLocation.transform;
-            isPlacing = true;
-        }
     }
 
     private void Update()
@@ -53,12 +41,59 @@ public class BuildingSystem : MonoBehaviour
             float snappedY = Mathf.Round(mousePosition.y / gridSize) * gridSize;
 
             buildingPrefabClone.transform.position = new Vector2(snappedX, snappedY);
+            buildingPrefabClone.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
 
-            if (Input.GetMouseButtonDown(0))
-            {   
-                buildingPrefabClone.GetComponent<BoxCollider2D>().isTrigger = false;
-                isPlacing = false;
+            if (Vector2.Distance(player.transform.position, buildingPrefabClone.transform.position) <= playerReach)
+            {
+                buildingPrefabClone.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = canBuildSprite;
+
+                if (IsBuildable())
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        buildingPrefabClone.layer = LayerMask.NameToLayer("Ground");
+                        buildingPrefabClone.GetComponent<SpriteRenderer>().sortingOrder = 0;
+                        buildingPrefabClone.GetComponent<BoxCollider2D>().isTrigger = false;
+                        buildingPrefabClone.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+                        isPlacing = false;
+                    }
+                }
+                else if (!IsBuildable())
+                {
+                    buildingPrefabClone.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = cantBuildSprite;
+                }
+            }
+            else if (Vector2.Distance(player.transform.position, buildingPrefabClone.transform.position) > playerReach)
+            {
+                buildingPrefabClone.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = cantBuildSprite;
             }
         }
-    }  
+    }
+
+    public void onButtonChoice(GameObject buildingPrefab)
+    {
+        if (!isPlacing)
+        {
+            buildingPrefabClone = Instantiate(buildingPrefab, (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            buildingPrefabClone.GetComponent<SpriteRenderer>().sortingOrder = 1;
+            buildingPrefabClone.GetComponent<BoxCollider2D>().isTrigger = true;
+            buildingPrefabClone.name = buildingPrefab.name;
+            buildingPrefabClone.transform.parent = clonesLocation.transform;
+            isPlacing = true;
+        }
+    }
+
+    private bool IsBuildable()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(buildingPrefabClone.transform.position, Vector2.one * gridSize / 2, 0);
+
+        foreach (Collider2D collider in colliders)
+        {    
+            if (collider.gameObject != buildingPrefabClone && !collider.isTrigger)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- Assets/Scripts/RemoveSprite.cs
+++ Assets/Scripts/RemoveSprite.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class RemoveSprite : MonoBehaviour
@@ -8,6 +9,7 @@ public class RemoveSprite : MonoBehaviour
     {
         if (gameObject.GetComponent<BoxCollider2D>().isTrigger == false)
         {
+
             if (Input.GetMouseButtonDown(1))
             {
                 Destroy(gameObject);
